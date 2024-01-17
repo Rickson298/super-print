9c877ba0aee6f4c4915ad724cec4bb9da11c5283
Rickson Oliveira <rickson.patrick13@gmail.com>
2024-01-16 21:54:42 -0300

    setup: create Electron setup

---
+++ main.js
@@ -0,0 +1,24 @@
+import { app, BrowserWindow } from 'electron'
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
